f7e61dc063d349787ff9c07f36f753faa6fb64fb
Nacho228 <6319@holbertonstudents.com>
2023-07-04 07:48:49 -0700

    Simple interactive command processor

---
+++ console/console.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Console for AirBnB"""
+import cmd
+
+
+class console(cmd.Cmd):
+    """Simple command processor
+
+    Args:
+        Cmd: Used to read the input
+        and execute a command
+    """
+    
+    def do_help(self, arg: str) -> bool:
+        """ 
+        Displays undocumented commands 
+        """
+        return super().do_help(arg)
+
+    def do_EOF(self, line):
+        """
+        Reaches end of file(quits the program)
+        """
+        return True
+    
+    def do_quit(self, line):
+        """
+        Exits the program soft, clean and smoothly
+        """
+        return self.do_EOF
+
+if __name__ == '__main__':
+    console().cmdloop()
+
+
