78fd98c95a9a875c92dbc8ab910a216421d8194d
GabiAcosta <acostagabriel1311@gmail.com>
2023-07-05 11:27:25 -0700

    created file storage engine

---
+++ models/engine/file_storage.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""for later"""
+import json
+
+
+class FileStorage:
+    """
+    Serializes instances to a JSON file and deserializes JSON file to instances
+    """
+    __file_path = "file.json"
+    __objects = {}
+    
+    def all(self):
+        """Returns the dictionary __objects"""
+        return self.__objects
+
+    def new(self, obj):
+        """Sets in __objects the obj with key <obj class name>.id"""
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
