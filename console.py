2bd4271d3bf32c5ea29525f5cc10cceeb156ced6
Nacho228 <6319@holbertonstudents.com>
2023-07-06 12:23:31 -0700

    Console.py do_create and do_show barely works

--- console.py
+++ console.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ Console for AirBnB"""
 import cmd
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage 
 
 
 class HBNBCommand(cmd.Cmd):
@@ -30,11 +32,52 @@ class HBNBCommand(cmd.Cmd):
         """
         return self.do_EOF
 
+    def do_create(self, line):
+        """ 
+        
+        """
+        if line is None:
+            print("** class name missing **")
+        elif line not in FileStorage.class_dict:
+            print("** class doesn't exist **")
+        else:
+            mew = FileStorage.class_dict[line]()
+            FileStorage.new(mew, mew)
+            FileStorage.save(self)
+            print(mew.id)
+    
+    def do_show(self, line):
+        """ 
+        
+        """
+        if line is None:
+            print("** class name missing **")
+        if line is not None:
+            args = line.split(" ")
+        else:
+            print("** class name doesn't exist **")
+        # if args[1] not in FileStorage.class_dict:
+        #     print("** class doesn't exist **")
+          
+        # if args[2] != "id":
+        #     print("** instance id missing **")
+        # else:
+        #     for k in FileStorage.__objects.keys():
+        #         id = k.split(".")
+        #         if id[1] != args[2]:
+        #             print("** no instance found **")
+        #         else:
+        #             self.__str__
+        
+        
+            
+
     def emptyline(self):
         """
         Do nothing when an empty line is entered
         """
         pass
+
 
 
 if __name__ == '__main__':
