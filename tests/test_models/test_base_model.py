76d494b58a71a692875e7aa29e9b39c14156761a
Gabriel <acostagabriel1311@gmai.com>
2023-07-08 12:25:44 -0700

    created tests for all classes

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,14 +1,27 @@
 #!/usr/bin/python3
+"""Unittest for BaseModel class"""
+import unittest
 from models.base_model import BaseModel
 
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+base = BaseModel()
+
+
+class TestBaseModel(unittest.TestCase):
+    
+    def test_init(self):
+        self.assertTrue(isinstance(base, BaseModel))
+
+    def test_save(self):
+        base.save()
+        self.assertNotEqual(base.created_at, base.updated_at)
+
+    def test_to_dict(self):
+        base_dict = base.to_dict()
+        self.assertEqual(base_dict["__class__"], "BaseModel")
+        self.assertIsInstance(base_dict["created_at"], str)
+        self.assertIsInstance(base_dict["updated_at"], str)
+
+
+if __name__ == "__main__":
+    unittest.main()
